542f08866bfed3e2016acb3782c41cd0dffa0582
olhaSlipushchenko <103112284+olhaSlipushchenko@users.noreply.github.com>
2022-11-23 21:35:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
       <NewCost onAddCost={addCostHandler} />
       <Costs costs={costs} />
       <p>
-        This is opencode, you can
+        This is opencode, you can 
         <a href="https://github.com/olhaSlipushchenko/Income_test.git">
           check it here
         </a>
